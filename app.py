64e2096701e635f15cc279bf5e2815850c7dd771
Mr-FaZ3a <ktarimouadh@gmail.com>
2025-01-26 13:16:27 +0100

    project basic

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, jsonify, render_template, redirect
+from cs50 import SQL
+
+app = Flask(__name__)
+
+db = SQL("sqlite:///database.db")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
